81f5fe33aa4a5432a3f12a4ea397e5cbbbe524c3
Mikhail_Egorkin <egorkin.mikhail@gmail.com>
2022-12-09 18:40:48 +0100

    Commit

--- src/main/java/org/example/db/QuestionRepository.java
+++ src/main/java/org/example/db/QuestionRepository.java
@@ -2,6 +2,8 @@ package org.example.db;
 
 import org.example.db.entities.QuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +21,9 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, String
 
     @Transactional
     QuestionEntity save(QuestionEntity questionEntity);
+
+    @Transactional
+    @Query(value = "SELECT * FROM question WHERE question_id = :themeId", nativeQuery = true)
+    Optional<QuestionEntity> getQuestionByName(@Param("themeId") String themeId);
 
 }
